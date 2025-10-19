02a95bc9acc82fa6afa3f970f1df833720c115ec
XiaoColorful <XColor_ful@outlook.com>
2025-10-19 17:00:39 +0800

    Port to Neoforge 1.20.x

---
+++ neoforge-compat/src/main/java/xiao/cbraddon/compat/neoforge/CbrAddonNeoforge.java
@@ -0,0 +1,18 @@
+package xiao.cbraddon.compat.neoforge;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLLoader;
+import xiao.battleroyale.api.common.McSide;
+import xiao.cbraddon.CbrAddon;
+
+@Mod(CbrAddon.MOD_ID)
+public class CbrAddonNeoforge {
+
+    public CbrAddonNeoforge() {
+        Dist dist = FMLLoader.getDist();
+        McSide mcSide = dist.isClient() ? McSide.CLIENT : McSide.DEDICATED_SERVER;
+
+        CbrAddon.init(mcSide);
+    }
+}
