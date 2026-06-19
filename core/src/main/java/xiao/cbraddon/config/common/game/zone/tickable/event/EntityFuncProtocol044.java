093055bced325b2ed3cc799327ed4ad5f1b1c99a
XiaoColorful <XColor_ful@outlook.com>
2026-06-19 12:03:52 +0800

    keep CBR framework

--- core/src/main/java/xiao/cbraddon/config/common/game/zone/tickable/event/EntityFuncProtocol044.java
+++ core/src/main/java/xiao/cbraddon/config/common/game/zone/tickable/event/EntityFuncProtocol044.java
@@ -1,4 +1,4 @@
-package xiao.cbraddon.config.common.game.zone.zonefunc.custom;
+package xiao.cbraddon.config.common.game.zone.tickable.event;
 
 import com.google.gson.JsonObject;
 import net.minecraft.world.phys.Vec3;
